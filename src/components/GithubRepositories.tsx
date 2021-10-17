d093e1431a07b1637eb114155385b5240ee50a56
JoshDaFishy <computergeek328@gmail.com>
2021-10-17 15:03:41 +0100

    started animating portfolio page

--- src/components/GithubRepositories.tsx
+++ src/components/GithubRepositories.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import axios from "axios"
+import { CSSTransition } from "react-transition-group"
 import GithubIcon from "../assets/Githubpage/GithubMain.svg"
 import JumpPage from "../assets/Githubpage/NextPage.svg"
+import ExpandArrows from "../assets/Githubpage/ExpandArrows.svg"
 import "../styles/GithubRepositories.scss"
 
 interface IRepo {
@@ -13,6 +15,7 @@ interface IState {
     repositories: IRepo[]
     pageNumber: number
     pageCount: number
+    maximisedRepo?: IRepo
 }
 
 class GithubRepositories extends React.Component<{}, IState> {
@@ -41,47 +44,106 @@ class GithubRepositories extends React.Component<{}, IState> {
         if (end > this.state.repositories.length) {
             end = this.state.repositories.length
         }
+
+        let RepoName = "BeforeGitHubRepo"
         
         for (let x = start; x < end; x++) {
             let repo = this.state.repositories[x]
-            result.push(
-                <div key={x} className="GitHubRepo">
-                    <a className="GithubSource" href={repo.html_url} ><img className="GithubSource" src={GithubIcon} alt="GithubIcon" /></a>
-                    <div className="repoText">
-                        <p>{repo.name}</p>
-                        <p className="githubrepoURL" >{repo.html_url}</p>
-                    </div>
-                </div>
-            )
+            
+            if (repo == this.state.maximisedRepo) {
+                result.push(<div key={x} className="ExpandedRepo">
+                    {this.renderMaximisedRepo()}
+                </div>)
+                RepoName = "AfterGitHubRepo"
+            }
+            else {
+                result.push(
+                    <CSSTransition
+                        in={this.state.maximisedRepo == undefined}
+                        timeout={3000}
+                        unmountOnExit
+                        className={RepoName}
+                        classNames={RepoName}
+                        onEnter={this.startAnim}
+                        onExit={this.exitAnim}
+                        key={x}
+                    >
+                        <div> 
+                            <a className="GithubSource" href={repo.html_url} ><img className="GithubSource" src={GithubIcon} alt="GithubIcon" /></a>
+                            <div className="repoText">
+                                <p>{repo.name}</p>
+                                <p className="githubrepoURL" >{repo.html_url}</p>
+                            </div>
+                            <img className="expandArrows" onClick={() => this.repoDropdown(repo)} src={ExpandArrows} alt="expand"/>
+                        </div>
+                    </CSSTransition>
+                )
+            }
         }
         return result
     }
 
+    startAnim = () => {
+        
+    }
+
+    exitAnim = () => {
+
+    }
+
+    repoDropdown = (repo: IRepo) => {
+        this.setState({ maximisedRepo: repo })
+    }
+
     decPage = () => {
         this.setState({pageNumber: this.state.pageNumber - 1})
+        this.setState({ maximisedRepo: undefined })
     }
 
     incPage = () => {
         this.setState({pageNumber: this.state.pageNumber + 1})
+        this.setState({ maximisedRepo: undefined })
     }
 
     firstPage = () => {
         this.setState({pageNumber: 0})
+        this.setState({ maximisedRepo: undefined })
     }
 
     lastPage = () => {
         this.setState({pageNumber: this.state.pageCount - 1})
+        this.setState({ maximisedRepo: undefined })
+    }
+
+    renderMaximisedRepo = () => {
+        if (!this.state.maximisedRepo) return
+        let repo = this.state.maximisedRepo
+        return (
+            <>
+                <div className="GitHubRepo">
+                        <a className="GithubSource" href={repo.html_url} ><img className="GithubSource" src={GithubIcon} alt="GithubIcon" /></a>
+                        <div className="repoText">
+                            <p>{repo.name}</p>
+                            <p className="githubrepoURL" >{repo.html_url}</p>
+                        </div>
+                </div>
+                <div className="flyout">
+                    a
+                </div>
+            </>
+        )
     }
 
     render = () => {
         return (
             <>
                 <div className="backgroundSquare">
+                    {/* {this.state.maximisedRepo ? this.renderMaximisedRepo() : this.renderRepos()} */}
                     {this.renderRepos()}
                 </div>
                 <div className="pageSelectorBackground">
-                        
-                        <img className="FirstPageButton" onClick={this.firstPage} src={JumpPage} alt="Jump to first page" />
+                    <img className="FirstPageButton" onClick={this.firstPage} src={JumpPage} alt="Jump to first page" />
+                    <div className="firstpage-decrementpage"></div>
                     {this.state.pageNumber > 0 &&
                         <button
                             onClick={this.decPage}
@@ -91,8 +153,12 @@ class GithubRepositories extends React.Component<{}, IState> {
                         </button>
                     }
                     
+                    <div className="decrementpage-currentpage"></div>
+
                     <div className="currentPage" >{this.state.pageNumber + 1}</div>
                     
+                    <div className="currentpage-incrementpage"></div>
+
                     {this.state.pageNumber < this.state.pageCount - 1 &&
                         <button
                             onClick={this.incPage}
@@ -102,6 +168,8 @@ class GithubRepositories extends React.Component<{}, IState> {
                         </button>
                     }
                     
+                    <div className="incrementpage-lastpage"></div>
+
                     <img className="LastPage" onClick={this.lastPage} src={JumpPage} alt="Jump to last page" />
                 </div>
                 <div className="BacktoHome">
